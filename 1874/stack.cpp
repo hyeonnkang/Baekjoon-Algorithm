868710f3e067cc8a0a6033825f895dfccee5ce5e
kang hyeon <hyeonkang.dev@gmail.com>
2021-10-12 21:10:09 +0900

    시간초과 풀이 실패

---
+++ 1874/stack.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+void printplus(){ cout << '+' << endl;}
+void printminus() {cout << '-' << endl;}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    stack<int> s;
+    vector<char> p; // + or - 를 출력을 결정하는 배열
+    s.push(1);
+    p.push_back('+');
+    int max = 1;    // 현재 스택에 들어갔던 가장 큰 수를 저장하는 변수
+
+    bool canMake = true;
+
+    // n개의 정수를 얻는다.
+    for(int input = 0; input < n; input++){
+        int k;
+        cin >> k;
+        if(canMake == false) continue;
+
+        if(s.empty() && k > max){  // 스택이 비었다면
+            int i = max + 1;
+            for(; i <= k; i++){    // i+1~k 까지 스택에 push
+                s.push(i);
+                p.push_back('+');
+            }
+            max = k;
+            s.pop();     // 현재 top에 있는 k를 pop 한다.
+            p.push_back('-');
+        }
+        else if(s.top() < k && k > max){    // top에 있는 숫자가 k 보다 작다면
+            int i = max + 1;
+            for(; i <= k; i++){    // i+1~k 까지 스택에 push
+                s.push(i);
+                p.push_back('+');
+            }
+            max = k;
+            s.pop();     // 현재 top에 있는 k를 pop 한다.
+            p.push_back('-');
+        }else if(s.top() == k){ // top이 바로 k 라면
+            s.pop();
+            p.push_back('-');
+        }else{
+            canMake = false;
+        }
+    }
+
+    if(canMake){
+        for(int i = 0; i < p.size(); i++){
+            if(p[i]=='+') printplus();
+            else printminus();
+        }
+    }else{
+        cout << "NO" << endl;
+    }
+}
